1f2515c2ed338b9fe35e8850aa4ce857418ea7af
Sammy16z <112364970+Sammy16z@users.noreply.github.com>
2023-07-15 12:05:32 -0400

    Add files via upload

--- MainFrame.py
+++ MainFrame.py
@@ -134,13 +134,13 @@ class MainFrame:
             self.exchange.colored_log('red', "Insufficient funds. Cannot execute trades.")
 
         while running:
-            print("iteration...")
 
             # Update the price_data dictionary with the latest price values
             for product_id in self.product_ids:
                 latest_price = await self.exchange.get_latest_price(product_id)
                 if latest_price is not None:
                     self.trade_bot.price_data[product_id] = latest_price
+                    self.exchange.colored_log('yellow', self.trade_bot.price_data[product_id])
 
                     # Execute the bot
                     await self.trade_bot.execute(product_id, amount)
